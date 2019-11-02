7576e3b0a0fe6aaf44fc342702980cb53f8e81f0
Dee Gao <dee.gao12@gmail.com>
2019-11-02 18:22:07 -0400

    Initial Commit

---
+++ Eldy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Eldy
+//
+//  Created by Dee Gao on 11/2/19.
+//  Copyright © 2019 Dee Gao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
